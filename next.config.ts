8fcd811560fad8d4f07feeb67c1e678e70a63e4d
neylorfm <neylor.prof@gmail.com>
2026-03-10 21:33:12 -0300

    fix(config): permite acesso via rede local no ambiente de desenvolvimento - Adicionada a propriedade  no [next.config.ts](cci:7://file:///home/neylor/Desenvolvimento/modulo_escolar/next.config.ts:0:0-0:0) para permitir requisições cross-origin do IP local () durante o desenvolvimento. - Isso resolve o problema onde o Next.js 15+ bloqueava a hidratação do client em dispositivos externos na mesma rede, fazendo a aplicação travar no estado de loading inicial.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["localhost", "192.168.0.93"],
 };
 
 export default nextConfig;
